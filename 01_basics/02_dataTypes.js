fa657cf47a22d930d48d18e69a58cc47c41ad514
Naveen <naveen.satish24@gmail.com>
2023-10-04 16:54:45 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use scrict"; // treat all JS code as newer version
+
+// alert("Hello") // We are using node js not browser
+
+console.log(3+3);
+
+let name = "naveen"
+let age = 32
+let isLoggedIn = true
+
+// number => 2^53 range
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
