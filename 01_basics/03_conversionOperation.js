0ffa55ec3330605080f5adaf89133951b9541eed
Naveen <naveen.satish24@gmail.com>
2023-10-11 14:34:10 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,7 +4,7 @@ let score2 = null
 let score3 = undefined
 let score4 = true
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
 let valueInNumber1 = Number(score1)
@@ -12,20 +12,44 @@ let valueInNumber2 = Number(score2)
 let valueInNumber3 = Number(score3)
 let valueInNumber4 = Number(score4)
 
-console.log(typeof valueInNumber);
-console.log(typeof valueInNumber1);
-console.log(typeof valueInNumber2);
-console.log(typeof valueInNumber3);
-console.log(typeof valueInNumber4);
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber1);
+// console.log(typeof valueInNumber2);
+// console.log(typeof valueInNumber3);
+// console.log(typeof valueInNumber4);
 
 
-console.log(valueInNumber);
-console.log(valueInNumber1);
-console.log(valueInNumber2);
-console.log(valueInNumber3);
-console.log(valueInNumber4);
+// console.log(valueInNumber);
+// console.log(valueInNumber1);
+// console.log(valueInNumber2);
+// console.log(valueInNumber3);
+// console.log(valueInNumber4);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+/* *************************** Operations ******************************* */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1= "Naveen"
+let str2 = " Spidy"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1+"2"+2);
+// console.log(1+2+"2");
+
+// console.log(true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+// console.table([num1,num2,num3])
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
