27ae47f1d7c67173c7dacfb25b1d7d3638e689e9
Naveen <naveen.satish24@gmail.com>
2023-10-11 14:18:18 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33"
+let score1 = "33abc"
+let score2 = null
+let score3 = undefined
+let score4 = true
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber1);
+console.log(typeof valueInNumber2);
+console.log(typeof valueInNumber3);
+console.log(typeof valueInNumber4);
+
+
+console.log(valueInNumber);
+console.log(valueInNumber1);
+console.log(valueInNumber2);
+console.log(valueInNumber3);
+console.log(valueInNumber4);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
