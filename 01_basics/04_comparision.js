d29b6cdc170615ebdf65bf339884aaeba283e595
Naveen <naveen.satish24@gmail.com>
2023-10-13 13:56:15 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined <= 0);
+
+// Avoid above comparisions always
+
+// === strict check => compares along with the data type
+
+console.log("2" === 2);
