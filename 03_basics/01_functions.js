6a22c56cf1f4e76ef4df965fd71b04aadc2c23b5
Naveen <naveen.satish24@gmail.com>
2023-10-16 15:00:30 +0000

    functions 1

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,4 +31,53 @@ function loginUserMessage(username = "default value"){ // if no arguement is pas
 
 // console.log(loginUserMessage("Naveen"))
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+// shopping cart example.. we dont know how many arguements are passed.
+
+function calculateCartPrice(num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(2,200,500));
+
+// use rest operator when we are not sure about number of arguements. rest and spread operators are same but names are taken 
+// based on where it is used
+// rest operator combines all the passed arguements and combines inside a package  
+
+// function calculateCartPrice(...num1){
+//     return num1 // returns an array
+// }
+// console.log(calculateCartPrice(2,200,500)); // [ 2, 200, 500 ]
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1// returns an array
+}
+
+// console.log(calculateCartPrice(2,200,500)); // [ 500 ]
+
+// using objects
+
+const user = {
+    username : "Naveen",
+    price: 200
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject(user)
+
+// using arrays
+
+const myNewArr = [200,400,600,1000]
+
+function returnSecondValue(anyArray){
+    // console.log(`Second value passed is ${anyArray[1]}`);
+    return anyArray[1]
+}
+
+const returnedValue = returnSecondValue(myNewArr)
+console.log(returnedValue);
