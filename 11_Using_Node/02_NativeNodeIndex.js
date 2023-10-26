5eafe427293f030c83db7705710dcb3a82ba75cf
Naveen <naveen.satish24@gmail.com>
2023-10-26 09:41:13 +0000

    node 2

--- 11_Using_Node/02_NativeNodeIndex.js
+++ 11_Using_Node/02_NativeNodeIndex.js
@@ -1,8 +1,14 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt",'Hello from Node Js',(err) => {
-    if(err){
-        throw err
-    }
-    console.log("The file has been saved.");
+// fs.writeFile("message.txt",'Hello from Node Js',(err) => {
+//     if(err){
+//         throw err
+//     }
+//     console.log("The file has been saved.");
+// })
+let readData;
+fs.readFile('message.txt','utf-8',(err,data)=>{
+    if (err) throw err;
+    readData = data;
 })
+console.log(readData);
